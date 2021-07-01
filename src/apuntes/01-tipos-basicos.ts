5bee3734b8b8e774f7156bcc5cc695b0bb3a9050
Carlos Guijarro Martín <carlosguijarromartin@gmail.com>
2021-07-01 21:07:28 +0200

    Tipos Basicos

---
+++ src/apuntes/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript =====
+    Documentación Oficial: https://www.typescriptlang.org/docs/handbook/basic-types.html
+*/
+
+let nombre: string = 'Carlos';
+let vida: number | string = 95;
+let estaVivo: boolean = true;
+
+vida = 'Full';
+
+console.log(nombre, vida, estaVivo)
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+/*
+    ===== Código de TypeScript =====
+    Documentación Oficial: https://www.typescriptlang.org/docs/handbook/basic-types.html
+*/
