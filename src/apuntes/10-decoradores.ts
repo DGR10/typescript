a35c366e131d1d7385da2bc25ba6f65bb6247521
Carlos Guijarro Martín <carlosguijarromartin@gmail.com>
2021-07-02 23:47:52 +0200

    decoradores

---
+++ src/apuntes/10-decoradores.ts
@@ -0,0 +1,29 @@
+/*
+    ===== Código de TypeScript =====
+    Documentación Oficial: https://www.typescriptlang.org/docs/handbook/basic-types.html
+    Decoradores: https://www.typescriptlang.org/docs/handbook/decorators.html#class-decorators
+*/
+
+function classDecorator<T extends { new (...args: any[]): {} }>(
+    constructor: T
+) {
+    return class extends constructor {
+        newProperty = "new property";
+        hello = "override";
+    };
+}
+
+@classDecorator
+class MiSuperClase {
+    public miPropiedad: string = 'ABC123';
+
+    imprimir() {
+        console.log('Hola Mundo');
+    }
+}
+
+console.log(MiSuperClase);
+
+const miClase = new MiSuperClase();
+
+console.log(miClase.miPropiedad);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 /*
     ===== Código de TypeScript =====
     Documentación Oficial: https://www.typescriptlang.org/docs/handbook/basic-types.html
+    Decoradores: https://www.typescriptlang.org/docs/handbook/decorators.html#class-decorators
 */
